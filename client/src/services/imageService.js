e8c6da3248b66b5dbf7e355caac548d65ffb8b7d
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-08 16:26:37 -0600

    added image service

---
+++ client/src/services/imageService.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3001/images';
+
+function getImages() {
+  return fetch(`${baseURL}`)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((e) => e);
+}
+
+function createImage(image) {
+  return fetch(`${baseURL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(image),
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((e) => e);
+}
+
+export const imageService = {
+  getImages,
+  createImage
+};
+
