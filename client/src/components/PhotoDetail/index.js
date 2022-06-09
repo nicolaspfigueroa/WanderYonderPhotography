6d056925cdf7d34ced8f5fd1f89c92fe109e64e3
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-09 14:34:40 -0600

    detail page routing rendered

--- client/src/components/PhotoDetail/index.js
+++ client/src/components/PhotoDetail/index.js
@@ -4,10 +4,13 @@ import { useParams } from 'react-router-dom';
 export default function PhotoDetail({ photos }) {
   const { title } = useParams();
 
+  const photo = photos.filter(image => image.title === title);
+
   return (
     <>
-    <div>PhotoDetail</div>
     <div className = "container">
+      <img src = {photo[0].url}></img>
+      <div>{photo[0].title}</div>
     </div>
     </>
   )
