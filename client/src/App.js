27d85b27bab1a07b93846fe0198a96090643dffe
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-09 09:17:12 -0600

    implemented getAllPhotos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Photos from './pages/Photos';
+import { imageService } from './services/imageService';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getAllPhotos();
+  }, []);
+
+  const getAllPhotos = async () => {
+    const { res, error } = await imageService.getImages();
+
+    if (!error) {
+      setPhotos(res);
+    }
+    else {
+      setError(res);
+    }
+  }
+
+
   return (
     <Router>
       <div className="App">
@@ -14,7 +35,7 @@ function App() {
           <Routes>
             <Route path="/" element = {<Home />} />
             <Route path="/about" element = {<About />} />
-            <Route path="/photos" element = {<Photos />} />
+            <Route path="/photos" element = {<Photos getAllPhotos = {getAllPhotos}/>} />
           </Routes>
         </div>
       </div>
--- client/src/pages/Photos.js
+++ client/src/pages/Photos.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import UploadForm from '../components/UploadForm';
 
-export default function Photos() {
+export default function Photos( { getAllPhotos }) {
   return (
     <>
     <div>Photos</div>
