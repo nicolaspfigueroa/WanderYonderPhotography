6a6f778797b20733d8610884b47a15e0eb2e54e6
nicolaspfigueroa <nicolaspfigueroa@gmail.com>
2022-06-13 10:24:29 -0600

    About page finished

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import './styles.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="about-container">
+      <div className = "image"></div>
+      <p className = "text">My name is Austin Bennett. I am a landscape photographer originally from Louisiana. 
+      Upon moving to the west I was inspired by the incredible wilderness scenes that the mountain and desert held, 
+      and began using every spare moment to capture them. In that process I have travelled to over 50 national parks 
+      in the United States, climbed all 58 14,000 foot peaks in Colorado (and several more besides), and witnessed some 
+      of the most moving moments that nature has to offer. If I'm not working my day job as a nurse, you can be 
+      sure I am in the outdoors chasing light and getting too little sleep! I shoot on a Canon R6, Canon RF 15-35mm f2.8, 
+      Canon RF 24-105mm f4, Canon RF 100-500mm, and DJI Mavic 3. Email me if you have any inquiries!
+      </p>
+    </div>
   )
 }
